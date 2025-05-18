c606b2e9c0f563dc69987d0058b95075f0f7a9a7
Joanne <linjoanne823@gmail.com>
2025-05-18 14:01:26 -0700

    Remove debug mode

--- app.py
+++ app.py
@@ -360,4 +360,4 @@ def directory():
     return render_template_string(HTML_TEMPLATE, filtered=filtered, search=search, tag=tag, industry=industry, location=location)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
